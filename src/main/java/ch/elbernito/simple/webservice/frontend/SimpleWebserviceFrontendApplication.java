0da78bb841650be6a9f9e47df43e10a364b0cb52
Bernhard Brunner <bernhard.brunner@sbb.ch>
2023-04-24 07:50:25 +0100

    initial data

---
+++ src/main/java/ch/elbernito/simple/webservice/frontend/SimpleWebserviceFrontendApplication.java
@@ -0,0 +1,13 @@
+package ch.elbernito.simple.webservice.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebserviceFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebserviceFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/elbernito/simple/webservice/frontend/SimpleWebserviceFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.elbernito.simple.webservice.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebserviceFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
